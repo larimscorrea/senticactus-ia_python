581a37c2ce4e970172c55908514999b395864df6
Larissa Corrêa <larimscorrea@gmail.com>
2024-06-05 11:36:58 -0300

    fazendo a página principal e o desenvolvimento web para tornar a IA um game de sentimentos

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Página Inicial</title>
+</head>
+<body>
+    <?php include 'header.html'; ?>
+    <main>
+        <p>Este é o conteúdo principal da página.</p>
+    </main>
+</body>
+</html>
